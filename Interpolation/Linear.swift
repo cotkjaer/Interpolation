5f74cc856de58b36a26f4f04ceeaa71307731438
christian otkjær <christian.otkjaer@gmail.com>
2017-01-27 12:31:23 +0100

    cosmetics and added LERP as a top-level function

--- Interpolation/Linear.swift
+++ Interpolation/Linear.swift
@@ -13,14 +13,29 @@ public protocol BaseLinearInterpolatable: FloatingPoint, ExpressibleByIntegerLit
     static func - (lhs: Self, rhs: Self) -> Self
 }
 
+/** Basic linear interpolation between two floating-point values
+ - parameter a: the lower end of the interpolated range
+ - parameter b: the upper end of the interpolated range
+ - parameter t: the "time", is supposed to be in [0;1], but over-/under-shooting can be desired in certain situations
+ - returns: interpolation of a and b by t (a * (1 - t) + b * t)
+ */
+@inline(__always)
+public func lerp<I: BaseLinearInterpolatable> (a: I, b: I, t: I) -> I
+{
+    return (a * (1 - t) + b * t)
+}
 
 /// LERP operator (◊ made with alt-shift-v)
 infix operator ◊ : MultiplicationPrecedence //{ associativity left precedence 160 }
 
-/// Basic linear interpolation between two floating-point values
-public func ◊ <I: BaseLinearInterpolatable> (ab: (I,I), t: I) -> I
+/** Basic linear interpolation between two floating-point values
+ - parameter ab: the tuppel (a,b) that should be interpolated
+ - parameter t: the "time", should be in [0;1]
+ - returns: interpolation of a and b by t
+ */
+public func ◊ <I: BaseLinearInterpolatable> (ab: (a: I, b: I), t: I) -> I
 {
-    return ab.0 * (1 - t) + ab.1 * t
+    return ab.a * (1 - t) + ab.b * t
 }
 
 /// Basic linear interpolation between X floating-point values (result will be of length `values.count - 1`)
